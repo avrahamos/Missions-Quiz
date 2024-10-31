f2ab0b996fdea2217b1952ab1a9d30368e14f7b8
avraham <2721avi@gmail.com>
2024-10-31 11:07:19 +0200

    add handleAddMission

--- src/components/AddMissinComponent.tsx
+++ src/components/AddMissinComponent.tsx
@@ -1,6 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Mission from '../models/missionModel';
+import missionStatus from "./utils/missionStatus";
 
-const AddMissinComponent = () => {
+interface Props {
+  onAdd: (mission: Mission) => void;
+}
+
+
+const AddMissinComponent:React.FC<Props> = ({onAdd}) => {
+    const URL = "https://reactexambackend.onrender.com/missions";
+    const KEY = "8176800";
+
+     const [name, setName] = useState("");
+     const [status, setStatus] = useState(missionStatus.Pending);
+     const [priority, setPriority] = useState("low");
+     const [description, setDescription] = useState("");
+
+const handleAddMission = async () => {
+  if (name && priority && description) {
+    const newMission = new Mission(name, status, priority, description);
+    try {
+      const response = await fetch(`${URL}/${KEY}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newMission),
+      });
+      const data = await response.json();
+      onAdd(data);
+      setName("");
+      setStatus(missionStatus.Pending);
+      setPriority("low");
+      setDescription("");
+    } catch (error) {
+      console.error("Error adding the mission:", error);
+    }
+  } else {
+    alert("All fields must be complete");
+  }
+};
+
   return (
     <div>AddMissinComponent</div>
   )
