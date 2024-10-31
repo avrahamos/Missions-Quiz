d94669e84df9578cdd4fff8eb6fbea235a47f8b7
avraham <2721avi@gmail.com>
2024-10-31 10:44:09 +0200

    add fetchMissions and handleDelete

--- src/components/MissionsGrideComponent.tsx
+++ src/components/MissionsGrideComponent.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Mission from "../models/missionModel";
 
-const MissionsGrideComponent = () => {
-  return (
-    <div>MissionsGrideComponent</div>
-  )
-}
+const URL = "https://reactexambackend.onrender.com/missions";
+const KEY = "8176800";
 
-export default MissionsGrideComponent
+const MissionsGridComponent = () => {
+  const [missions, setMissions] = useState<Mission[]>([]);
+
+  useEffect(() => {
+    const fetchMissions = async () => {
+      try {
+        const response = await fetch(`${URL}/${KEY}`);
+        const data = await response.json();
+        setMissions(data);
+      } catch (error) {
+        console.error("Error loading tasks:", error);
+      }
+    };
+    fetchMissions();
+  }, []);
+
+  const handleDelete = async (id: string) => {
+    try {
+      await fetch(`${URL}/${KEY}/${id}`, { method: "DELETE" });
+      setMissions(missions.filter((mission) => mission.id !== id));
+    } catch (error) {
+      console.error("Error deleting mission:", error);
+    }
+  };
+
+  return;
+  <div>MissionsGridComponent</div>;
+};
+
+export default MissionsGridComponent;
